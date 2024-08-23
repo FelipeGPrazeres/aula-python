aaa61b967d12c67e5de807cb0776a1cc4f9c9391
FelipeGPrazeres <felipegalvaoprazeres@gmail.com>
2024-08-22 21:42:36 -0300

    fluxograma no python 22/08

---
+++ programacao/fluxograma.py
@@ -0,0 +1,10 @@
+numero = int(input("Qual o número? "))
+def verEnt(numero):
+    if numero > 0:
+        return "Seu número é positivo"
+    elif numero < 0:
+        return "Seu número é negativo"
+    else:
+        return "Seu número é zero"
+verEnt(numero)
+print(verEnt(numero))
