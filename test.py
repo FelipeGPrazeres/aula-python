319d52cf58a1ccc2af79fa4275ecb1fc191aea8a
FelipeGPrazeres <felipegalvaoprazeres@gmail.com>
2024-08-22 21:39:55 -0300

    Positivo, negativo ou zero

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print("Hello World")
+numero = int(input("Qual o número? "))
+def verEnt(numero):
+    if numero > 0:
+        return "Seu número é positivo"
+    elif numero < 0:
+        return "Seu número é negativo"
+    else:
+        return "Seu número é zero"
+verEnt(numero)
+print(verEnt(numero))
