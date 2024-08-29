9ddb8b52fef02974a9cf4f9d5071fc74fcb917a2
FelipeGPrazeres <felipegalvaoprazeres@gmail.com>
2024-08-28 21:41:13 -0300

    operações aritmétricas

---
+++ programacao/operacoesaritmeticas.py
@@ -0,0 +1,45 @@
+a = 5
+b = 3
+c = 4
+
+# Adição (+)
+resultado = a + b + c
+print(f"Soma: {resultado}")
+print("####################")
+#Subtração (-)
+resultado = c - b
+print(f"Subtração : {resultado}")
+print("####################")
+#Multiplicação (*)
+resultado = c * b
+print(f"Multiplicação : {resultado}")
+print("####################")
+#Divisão (/)
+resultado = c / b
+print(f"Divisão : {resultado}")
+print("####################")
+#Divisão Inteira (//)
+resultado = c // b
+print(f"Divisão Inteira : {resultado}")
+print("####################")
+#Resto da Divisão (%)
+resultado = c % b
+print(f"Resto da Divisão : {resultado}")
+print("####################")
+#Expoente (**)
+resultado = c ** b
+print(f"Expoente : {resultado}")
+print("####################")
+#Texto
+resultado = "vi" + "ado"
+print(f"Texto : {resultado}")
+print("####################")
+saldoIni = 50
+custorefri = 8
+custopao = 4
+dela = 39.90
+
+valorFim = (custorefri*2) + custopao + ((dela / 1000)* 300)
+
+saldoFinal = saldoIni - valorFim
+print(f"José chegou com R${saldoIni} e saiu com R${saldoFinal}")
