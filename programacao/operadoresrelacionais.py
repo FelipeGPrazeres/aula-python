6263d2f468c2454d6079ffb97b31aa137fa17422
FelipeGPrazeres <felipegalvaoprazeres@gmail.com>
2024-08-28 21:58:06 -0300

    operadores relacionais

---
+++ programacao/operadoresrelacionais.py
@@ -0,0 +1,20 @@
+a = 5
+b = 3
+#igualdade
+resultado = a == b
+print(resultado)
+#diferente
+resultado = a != b
+print(resultado)
+#maior que
+resultado = a > b
+print(resultado)
+#menor que
+resultado = a < b
+print(resultado)
+#maior igual que
+resultado = a >= b
+print(resultado)
+#menor igual que
+resultado = a <= b
+print(resultado)
