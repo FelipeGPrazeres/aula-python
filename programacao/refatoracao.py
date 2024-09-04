5c710e6fd73f96c41b4de551b51f29f978d5fb2f
FelipeGPrazeres <felipegalvaoprazeres@gmail.com>
2024-09-04 19:42:26 -0300

    refatoracao, exercicio antes da refatoracao

---
+++ programacao/refatoracao.py
@@ -0,0 +1,15 @@
+#antes
+def calc(a,b):
+    return a*b
+x = 10
+y = 20
+resultado = calc(x,y)
+print(resultado)
+
+#depois
+def calcular_area(largura, altura):
+    return largura * altura
+largura = 10
+altura = 20
+area = calcular_area(largura, altura)
+print(area)
