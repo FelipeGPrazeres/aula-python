8ba704bbba440b2d8f4a4acf81525bed70f0c46e
FelipeGPrazeres <felipegalvaoprazeres@gmail.com>
2024-09-04 21:09:34 -0300

    Exercício de refatoração 2 refatorado

--- programacao/exercicio2refatoracao.py
+++ programacao/exercicio2refatoracao.py
@@ -1,8 +1,22 @@
-idade = 25
+#Variáveis definidas
+idade = int(input("Qual sua idade?"))
 categoria = "Indefinada"
+
+#Definir como criança caso for menor de 13
 if idade < 13:
     categoria = "Criança"
+
+#Definir como adolescente se for menor de 18 e maior de 13
 elif idade >= 13 and idade < 18:
     categoria = "Adolescente"
 
-print("A pessoa é classificada como: ", categoria)
+#Definir como adulto se for maior de 18 e menor de 60
+elif idade >= 18 and idade < 60:
+    categoria = "Adulto"
+
+#Definir como idoso se for maior de 60
+else:
+    categoria = "Idoso"
+    
+#Printar categoria resultante
+print(f"Você é classificada como: {categoria}")
